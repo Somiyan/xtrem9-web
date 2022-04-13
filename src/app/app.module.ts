b55886f9e692e36968bec8c2ff4d3923f87fb5ed
somiyan <somiyanguchait@idcletech.com>
2022-04-13 16:51:46 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ButtonModule} from 'primeng/button';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { AppMainComponent } from './app-main/app-main.component';import { SidebarComponent } from './components/sidebar/sidebar.component';
+;
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
