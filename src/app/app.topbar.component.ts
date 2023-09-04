273cdaff8ca5760395a086a2e7829142c3e686f0
somiyan <somiyanguchait@idcletech.com>
2023-09-04 13:53:55 +0530

    Cosmetic changes

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy } from '@angular/core';
 import { AppMainComponent } from './app.main.component';
 import { Subscription } from 'rxjs';
 import { MenuItem } from 'primeng/api';
+import { AuthService } from './service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-topbar',
@@ -10,6 +12,33 @@ import { MenuItem } from 'primeng/api';
 export class AppTopBarComponent {
 
     items: MenuItem[];
+    userRole: number = 1;
+    roleName: string = 'Customer'
 
-    constructor(public appMain: AppMainComponent) { }
+    constructor(public appMain: AppMainComponent, private _auth: AuthService,private router: Router ) { 
+        this.userRole = this._auth.userDetails.userRole || 1;
+
+        switch (this.userRole) {
+            case 1:
+                this.roleName = "Customer"
+                break;
+                case 2:
+                    this.roleName = "Agent"
+                    break;
+                    case 3:
+                        this.roleName = "Manager"
+                        break;
+                        case 4:
+                            this.roleName = "Director"
+                            break;        
+            default:
+                this.roleName = "Customer"
+                break;
+        }
+    }
+
+    public logout(){
+        localStorage.removeItem('jwtToken');
+        this.router.navigate(['login']);
+    }
 }
